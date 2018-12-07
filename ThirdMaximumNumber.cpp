4790eecb1f61abb490e37ac377ea4d449262b7bf
maoping.li <maoping.li@nio.com>
2018-12-07 17:45:59 +0800

    unfinished ThirdMaximumNumber.cpp

--- ThirdMaximumNumber.cpp
+++ ThirdMaximumNumber.cpp
@@ -8,7 +8,7 @@ int thirdMax(vector<int>& nums);
 
 int main()
 {
-	int temp[3]={1,2,-2147483648};
+	int temp[]={5,2,4,1,3,6,0};
 	vector<int> numbers;
 	int result;
 
@@ -23,12 +23,10 @@ int main()
 	return 0;
 }    
 
-int thirdMax(vector<int>& nums) {
+/*int thirdMax(vector<int>& nums) {
 	vector<int> v;
-    int i=0,j,k,temp,log;
-    v.push_back(nums[i]);
-    while(++i<nums.size())
-    {
+	int i;
+	for (i=0;i<nums.size();i++)
     	if (v.size()<3)
     	{
     		log = 1;
@@ -49,9 +47,33 @@ int thirdMax(vector<int>& nums) {
     						v[k] = temp;
     					}
     	}
+    return 0;
+}
+*/
+
+int thirdMax(vector<int>& nums) {
+	vector<int> v;
+    int i=0,j,k,temp,log;
+    v.push_back(nums[i]);
+    while(++i<nums.size())
+    {
+/*    	if (v.size()<3)
+/*    	{*/
+/*    		log = 1;
+			for (j=0;j<v.size();j++)
+    			if (v[j]==nums[i]) {
+    				log = 0;
+//    				break;
+    			} */
+    		if (nums[i]>v[0]) v.insert(v.begin(),nums[i]);
+    		else if (nums[i]>v[1] && nums[i]!=v[0]) v.insert(v.begin()+1,nums[i]);
+			else if (nums[i]>v[2] && nums[i]!=v[1]) v.insert(v.begin()+2,nums[i]);
+/*
+   
+    	}
     	else if (nums[i]>v[0]) v.insert(v.begin(),nums[i]);
     	else if (nums[i]>v[1] && nums[i]<v[0]) v.insert(v.begin()+1,nums[i]);
-    	else if (nums[i]>v[2] && nums[i]<v[1]) v.insert(v.begin()+2,nums[i]);
+    	else if (nums[i]>v[2] && nums[i]<v[1]) v.insert(v.begin()+2,nums[i]);*/
     }
     if (v.size()<3)
     	return v[0];
