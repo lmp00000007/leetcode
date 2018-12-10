37fc4e5697257cfb1ed70315065511d2291a3ee9
Desktop at Home <limaoping1231@hotmail.com>
2018-12-10 23:34:23 +0800

    12.10

--- 561.cpp
+++ 561.cpp
@@ -6,33 +6,33 @@ using namespace std;
 
 int main()
 {
-	int temp[4]={0,0,1,5};
+	int temp[4]={0,3,9,5};
 	vector<int> numbers;
 	numbers.insert(numbers.begin(),temp,temp + sizeof(temp)/4);
 
 	int i;
 	for (i=0;i<numbers.size();i++)
 		cout << numbers[i] << " ";
+		cout << endl;
 	cout << arrayPairSum(numbers) << endl;
 	return 0;
 }    
 
     int arrayPairSum(vector<int>& nums) {
-    	int i,j,ans=0,positive=-1;
-    	int temp;
+    	int i,j,ans=0;
+    	int temp,sign=-1;
     	for (i=0;i<nums.size()-1;i++)
     	{
-    		positive = -positive;
+			sign = -sign;
     		for (j=i+1;j<nums.size();j++)
-    		{	
     			if (nums[i]>nums[j])
     			{
     				temp = nums[i];
     				nums[i] = nums[j];
     				nums[j] = temp;
     			}
-    			if (positive==1) ans = ans + nums[i];
-    		}
+			if (sign==1) ans += nums[i];
+							cout << ans << endl;
     	}
     	return ans;
 	}
